027474958dc94f1c94d3260abe102fa168c9c355
AdrySky <adry.aqil@skymind.my>
2021-07-21 16:42:38 +0800

    Add and change icon

--- src/index.ts
+++ src/index.ts
@@ -17,11 +17,29 @@ const extension: JupyterFrontEndPlugin<void> = {
     const style = 'jupyterlab_sky_theme/index.css';
 
     manager.register({
-      name: 'jupyterlab_sky_theme',
+      name: 'JupyterLab Sky',
       isLight: true,
       load: () => manager.loadCSS(style),
       unload: () => Promise.resolve(undefined)
     });
+
+  //   let head = document.head || document.getElementsByTagName('head')[0];
+
+  //   function changeFavicon(src: string) {
+  //     let link = document.createElement('link'),
+  //      oldLink = document.getElementById('dynamic-favicon');
+  //     link.id = 'dynamic-favicon';
+  //     link.rel = 'icon';
+  //     link.type = 'image/x-icon';
+  //     link.href = src;
+  //     if (oldLink) {
+  //       head.removeChild(oldLink);
+  //     }
+  //       head.appendChild(link);
+  //   }
+  // changeFavicon('file:///C:/Users/user/jupyterlab_sky_theme/style/images/cloud.svg');
+  // changeFavicon('http://www.google.com/favicon.ico');
+  // changeFavicon('https://stackoverflow.com/favicon.ico');
   }
 };
 
